7f66ff42016de864c8d12f08b6d616a80edce8a7
Ken Ling <ken88ling@gmail.com>
2019-04-18 07:32:16 +0800

    use .net library EventHandler

--- EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/VideoEncoder.cs
@@ -16,8 +16,17 @@ namespace EventsAndDelegates
         // 2- Define an event based on that delegate
         // 3- Raise the event
 
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
-        public event VideoEncodedEventHandler VideoEncoded;
+        //public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        //public event VideoEncodedEventHandler VideoEncoded;
+
+        // using standard .net library
+        // EventHandler
+        // EventHandler<TEventArgs>
+
+        //public event EventHandler VideoEncoded; //normal form if you didn't have pass object
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+        
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video...");
